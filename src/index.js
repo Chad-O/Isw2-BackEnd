0f5060b6de68726876ef34a78fc17bad5254dbe2
Mauricio <mauriciocisneroslaule@gmail.com>
2022-09-02 21:17:11 -0500

    Cors pg express

--- src/index.js
+++ src/index.js
@@ -7,19 +7,13 @@ import {InicializarModelos} from './models/MODELS.js'
 
 async function main() {
     try {
-        //sequelize.drop();
+        sequelize.drop();
         InicializarModelos();
         await sequelize.sync({force : true});
         const queryInterface = sequelize.getQueryInterface();
 
-        //queryInterface.addColumn('CITAS', 'LINK', { type: DataTypes.STRING });
-        // queryInterface.addColumn('CITAS', 'DIRECCION', { type: DataTypes.STRING });
-        //queryInterface.addColumn('CITAS', 'MONTO', { type: DataTypes.INTEGER });
-        //queryInterface.addColumn('HISTORIA_CLINICA', 'FECHA', { type: DataTypes.DATEONLY });
-        //queryInterface.addColumn('MEDICOS', 'FG_PRESENCIAL', { type: DataTypes.BOOLEAN });
-        //queryInterface.addColumn('MEDICOS', 'FG_VIRTUAL', { type: DataTypes.BOOLEAN });
         console.log("Conexión realizada con éxito")
-        var PORT = process.env.PORT || 4000;
+        var PORT = 4000;
         app.listen(PORT);
         console.log("servidor corriendo en el puerto", PORT);
     } catch (error) {
