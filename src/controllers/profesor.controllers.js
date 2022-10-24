be599f3f42976db2bd8bc4a8786fb4abaed16d51
0knotok <20213134@aloe.ulima.edu.pe>
2022-10-24 17:39:37 -0500

    LO LOGRÉ

--- src/controllers/profesor.controllers.js
+++ src/controllers/profesor.controllers.js
@@ -4,9 +4,9 @@ const fs = require("fs");
 //Subir Archivo
 const subirArchivo = async (req, res) => {
     const dataPdf = req.body;
-    let nombre = dataPdf.formName
+    console.log("-------------------------------------------------------------------------------pinga de burra")
+    let nombre = dataPdf.formName;
     let pdfDoc = dataPdf.formData;
-    console.log(nombre);
     let buff = new Buffer.from(pdfDoc, 'base64');
     fs.writeFileSync('./src/files/'+nombre, buff);
 }
--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ app.set("port", process.env.PORT || 4000);
 // Middlewares
 app.use(cors());
 app.use(morgan('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true }));
 app.use(session({
   secret:'12345',
   resave: true,
@@ -24,13 +24,6 @@ app.use(session({
 //Routes
 app.use(routerVisitante);
 app.use(routerProfesor);
-
-// Aumentar límite al req
-app.use(bodyParser.json({ limit: '50mb' }))
-app.use(bodyParser.urlencoded({
-  limit: '50mb',
-  extended: false,
-}))
 
 app.listen(app.get("port"));
 console.log('Server on port', app.get("port"));
