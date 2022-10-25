ee000d1b6757a238c993cffe7fa4c2db08b882af
0knotok <20213134@aloe.ulima.edu.pe>
2022-10-24 21:47:58 -0500

    falta poo

--- src/controllers/profesor.controllers.js
+++ src/controllers/profesor.controllers.js
@@ -1,14 +1,18 @@
 const pool = require('../db');
 const fs = require("fs");
+const { Console } = require('console');
 
 //Subir Archivo
 const subirArchivo = async (req, res) => {
     const dataPdf = req.body;
-    console.log("-------------------------------------------------------------------------------pinga de burra")
     let nombre = dataPdf.formName;
     let pdfDoc = dataPdf.formData;
+    let idCurso = dataPdf.formIdCurso;
+    console.log(idCurso);
     let buff = new Buffer.from(pdfDoc, 'base64');
     fs.writeFileSync('./src/files/'+nombre, buff);
+
+    const result = await pool.query('INSERT INTO "MATERIAL" ("ID_CURSO", "LINK") VALUES ($1, $2)',[idCurso, nombre]);
 }
 
 
