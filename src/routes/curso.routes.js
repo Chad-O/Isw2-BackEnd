7b99f8e5c90614b6e442e64b1944984f240be5d3
Mauricio <mauriciocisneroslaule@gmail.com>
2022-10-25 21:39:56 -0500

    unamas

--- src/routes/curso.routes.js
+++ src/routes/curso.routes.js
@@ -6,6 +6,6 @@ const routerCurso = Router();
 
 routerCurso.get('/curso/mostrarCursos', mostrarCursos);
 
-routerCurso.get('curso/cursosUsuario/:id', cursosUsuario);
+routerCurso.get('/curso/cursosUsuario/:id', cursosUsuario);
 
 module.exports = routerCurso;
